f2cdd6694c09a83b39d6def9902d371622317953
Ertugrul Söylemez <esz@posteo.de>
2016-08-22 20:58:11 +0200

    Switched to BSD-3.

--- default.nix
+++ default.nix
@@ -1,13 +1,10 @@
-{ mkDerivation, base, containers, smallcheck, stdenv, tasty
-, tasty-smallcheck
-}:
+{ mkDerivation, base, containers, stdenv }:
 mkDerivation {
   pname = "predictive";
   version = "0.1.0";
   src = ./.;
   libraryHaskellDepends = [ base containers ];
-  testHaskellDepends = [ base smallcheck tasty tasty-smallcheck ];
   homepage = "https://github.com/esoeylemez/predictive";
   description = "Predict the future, backtrack on failure";
-  license = stdenv.lib.licenses.asl20;
+  license = stdenv.lib.licenses.bsd3;
 }
